b439ee52158fea0cd321fb32985ea47c5ab27d03
Niraj Kumar <86815759+niraj9504@users.noreply.github.com>
2022-06-07 18:44:52 +0530

    Update rotate matrix.cpp

--- rotate matrix.cpp
+++ rotate matrix.cpp
@@ -18,7 +18,22 @@ public:
         }
     }
 };
-//leetcode solution 2
+//leetcode solution 2 :- y'=ycos-xsin
+void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        int a = 0;
+        int b = n-1;
+        while(a<b){
+            for(int i=0;i<(b-a);++i){
+                swap(matrix[a][a+i], matrix[a+i][b]);
+                swap(matrix[a][a+i], matrix[b][b-i]);
+                swap(matrix[a][a+i], matrix[b-i][a]);
+            }
+            ++a;
+            --b;
+        }
+    }
+
 //codestudio solution
 #include <bits/stdc++.h> 
 
