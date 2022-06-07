6d4b2dedd147955f656edf2ee481f7462ef29a10
Niraj Kumar <86815759+niraj9504@users.noreply.github.com>
2022-06-07 23:33:49 +0530

    Update missing and repeating no.cpp

--- missing and repeating no.cpp
+++ missing and repeating no.cpp
@@ -17,3 +17,32 @@ pair<int,int> missingAndRepeating(vector<int> &nums, int n)
     return {x,y};
 }
 //try with extra space
+//m1-> bit manupulation(xor) **imp
+//try
+
+//m2->vvi
+#include <bits/stdc++.h> 
+
+pair<int,int> missingAndRepeating(vector<int> &nums, int n)
+{
+	int x,y,i=0;
+    while(i<n){
+        if(nums[i]!=nums[nums[i]-1]){
+            swap(nums[i],nums[nums[i]-1]);
+        }
+        else{
+            i++;
+        }
+    }
+    //for(auto it:nums)cout<<it<<" ";cout<<endl;
+    for(int i=0;i<n;i++){
+        if(nums[i]!=i+1){
+            x=i+1;
+            y=nums[i];
+            break;
+        }
+    }
+    return {x,y};
+}
+
+
