5dd5b1b5b85b1581d282c41d9069808a261561c3
Niraj Kumar <86815759+niraj9504@users.noreply.github.com>
2022-06-07 15:38:32 +0530

    Create rotate matrix.cpp

---
+++ rotate matrix.cpp
@@ -0,0 +1,105 @@
+//leetcode solution 1
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        //transpose +reverse=rotated
+        int n=matrix.size();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(j>i){
+                    swap(matrix[i][j],matrix[j][i]);
+                }
+            }
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n/2;j++){
+                swap(matrix[i][j],matrix[i][n-1-j]);
+            }
+        }
+    }
+};
+//leetcode solution 2
+//codestudio solution
+#include <bits/stdc++.h> 
+
+void rotateMatrix(vector<vector<int>> &mat, int n, int m)
+{
+    // similar to spiral matrix
+    int rs=0,cs=0,prev,curr;
+    while(rs<n && cs<m){
+        if(rs==n-1 || cs==m-1){
+            break;
+        }
+        prev=mat[rs+1][cs];
+        for(int i=cs;i<m;i++){
+            curr=mat[rs][i];
+            mat[rs][i]=prev;
+            prev=curr;
+        }
+        rs++;
+        for(int i=rs;i<n;i++){
+            curr=mat[i][m-1];
+            mat[i][m-1]=prev;
+            prev=curr;
+        }
+        m--;
+        if(rs<n){//no need
+            for(int i=m-1;i>=cs;i--){
+                curr=mat[n-1][i];
+                mat[n-1][i]=prev;
+                prev=curr;
+            }
+        }
+        n--;
+        if(cs<m){//no need
+            for(int i=n-1;i>=rs;i--){
+                curr=mat[i][cs];
+                mat[i][cs]=prev;
+                prev=curr;
+            }
+        }
+        cs++;
+    }
+
+}
+//no need of taking if cases 
+#include <bits/stdc++.h> 
+
+void rotateMatrix(vector<vector<int>> &mat, int n, int m)
+{
+    // similar to spiral matrix
+    int rs=0,cs=0,prev,curr;
+    while(rs<n && cs<m){
+        if(rs==n-1 || cs==m-1){
+            break;
+        }
+        prev=mat[rs+1][cs];
+        for(int i=cs;i<m;i++){
+            curr=mat[rs][i];
+            mat[rs][i]=prev;
+            prev=curr;
+        }
+        rs++;
+        for(int i=rs;i<n;i++){
+            curr=mat[i][m-1];
+            mat[i][m-1]=prev;
+            prev=curr;
+        }
+        m--;
+            for(int i=m-1;i>=cs;i--){
+                curr=mat[n-1][i];
+                mat[n-1][i]=prev;
+                prev=curr;
+            }
+        n--;
+            for(int i=n-1;i>=rs;i--){
+                curr=mat[i][cs];
+                mat[i][cs]=prev;
+                prev=curr;
+            }
+        cs++;
+    }
+
+}
+
+//Try it by recursion 
