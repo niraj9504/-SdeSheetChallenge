25ef70f7e8b94a8ac80a7435f716cc51d005f17c
Niraj Kumar <86815759+niraj9504@users.noreply.github.com>
2022-06-07 21:52:17 +0530

    Update find the duolicate no.cpp

--- find the duolicate no.cpp
+++ find the duolicate no.cpp
@@ -1,1 +1,9 @@
-
+//method 1 (learn it) o(n) ** see all solutions from leetcode 
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        while(nums[0]!=nums[nums[0]])swap(nums[0],nums[nums[0]]);
+        return nums[0];
+    }
+};
+//method 2 sorting t.c=O(nlogn) s.c=O(logn) in worst case for sorting 
