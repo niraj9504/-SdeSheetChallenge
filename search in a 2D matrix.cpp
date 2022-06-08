7b060f751bae5117db005d009dac8923aefc8686
Niraj Kumar <86815759+niraj9504@users.noreply.github.com>
2022-06-08 15:37:04 +0530

    Update search in a 2D matrix.cpp

--- search in a 2D matrix.cpp
+++ search in a 2D matrix.cpp
@@ -14,3 +14,20 @@ public:
         return false;
     }
 };
+//try search in a 2d mat ii from leetcode (try logm+lonn time complexity) also
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& mat, int t) {
+        //start from top right corner
+        int m=mat.size(),n=mat[0].size();
+        if(m<1 || n<1)return false;
+        if(t<mat[0][0] || t>mat[m-1][n-1])return false;
+        int i=0,j=n-1;
+        while(i<m && j>=0){
+            if(mat[i][j]==t)return true;
+            else if(mat[i][j]>t)j--;
+            else i++;
+        }
+        return false;
+    }
+};
