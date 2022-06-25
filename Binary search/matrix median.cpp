0a16730179a1d2eb38671ebcb5c83b405709e5ba
Niraj Kumar <86815759+niraj9504@users.noreply.github.com>
2022-06-25 21:18:49 +0530

    Create matrix median.cpp

---
+++ Binary search/matrix median.cpp
@@ -0,0 +1,37 @@
+//m1(binary search)
+int Solution::findMedian(vector<vector<int> > &matrix) {
+    int n=matrix.size();  //row
+    int m=matrix[0].size(); //column
+    long long reqElements=(long long)(n*m)/2;    //x<=median<=x -->then median
+    int low=1,high=1e9;
+    while(low<=high){
+        int mid=(low+high)/2;
+        long long x=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(matrix[i][j]<=mid){
+                    x++;
+                }
+            }
+        }
+        //if(x==reqElements)ans= mid;
+        if(x<=reqElements){
+            low=mid+1;
+        }
+        else{
+            high=mid-1;
+        }
+    }
+    return low;
+}
+//m2 (easy)
+int Solution::findMedian(vector<vector<int> > &A) {
+    int m=A.size(),n=A[0].size();
+    for(int r=m-1;r>0;r--){
+        for(int c=0;c<n;c++){
+            A[0].push_back(A[r][c]);
+        }
+    }
+    sort(A[0].begin(),A[0].end());
+    return A[0][(m*n/2)];
+}
