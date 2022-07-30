8f064713a357f6abc8f14a3d42b7a8c567960018
Niraj Kumar <86815759+niraj9504@users.noreply.github.com>
2022-07-30 12:45:07 +0530

    Create Compare Version Numbers.cpp

---
+++ string part ii/Compare Version Numbers.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int compareVersion(string v1, string v2) {
+        int m=v1.size(),n=v2.size();
+        int i=0,j=0;
+        while(i<m || j<n){
+            int num1=0,num2=0;
+            while(i<m && v1[i]!='.'){
+                num1=num1*10+(v1[i]-'0');
+                i++;
+            }
+            while(j<n && v2[j]!='.'){
+                num2=num2*10+(v2[j]-'0');
+                j++;
+            }
+            if(num1>num2)return 1;
+            else if(num1<num2)return -1;
+            i++;j++;
+        }
+        return 0;
+    }
+};
