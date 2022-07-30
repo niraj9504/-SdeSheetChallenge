9cb9d453cbc34144ca44e8ad445f001850410edb
Niraj Kumar <86815759+niraj9504@users.noreply.github.com>
2022-07-30 20:11:51 +0530

    Create Preorder Traversal.cpp

---
+++ Binary Tree/Preorder Traversal.cpp
@@ -0,0 +1,13 @@
+//also check for iterative method
+void solve(TreeNode* root,vector<int> &ans){
+    if(root==NULL)return;
+    ans.push_back(root->data);
+    solve(root->left,ans);
+    solve(root->right,ans);
+}
+vector<int> getPreOrderTraversal(TreeNode *root)
+{
+    vector<int> ans;
+    solve(root,ans);
+    return ans;
+}
